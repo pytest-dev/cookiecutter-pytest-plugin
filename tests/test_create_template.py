fee103465b371a4c38f3285fb24ba3eeb852b859
Raphael Pierzina <raphael@hackebrot.de>
2015-07-29 02:15:47 +0200

    Implement tests to verify the template

---
+++ tests/test_create_template.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_create_template
+--------------------
+"""
+
+
+import os
+import pip
+import pytest
+import shutil
+import subprocess
+
+TEMPLATE = os.path.realpath('.')
+
+
+@pytest.fixture(autouse=True)
+def clean_tmp_dir(tmpdir, request):
+    """
+    Remove the project directory that is created by cookiecutter during the
+    tests.
+    """
+    tmp_cwd = tmpdir.mkdir('cookiecutter_out')
+    os.chdir(str(tmp_cwd))
+
+    def remove_project_dir():
+        if os.path.isdir('pytest-foobar'):
+            shutil.rmtree('pytest-foobar')
+    request.addfinalizer(remove_project_dir)
+
+
+def test_run_cookiecutter_and_plugin_tests(testdir):
+    try:
+        subprocess.check_call(['cookiecutter', '--no-input', TEMPLATE])
+    except subprocess.CalledProcessError as e:
+        pytest.fail(e)
+
+    project_root = 'pytest-foobar'
+    assert os.path.isdir(project_root)
+
+    os.chdir(str(project_root))
+    pip.main(['install', '.'])
+
+    if testdir.runpytest().ret != 0:
+        pytest.fail('Error running the tests of the newly generated plugin')
