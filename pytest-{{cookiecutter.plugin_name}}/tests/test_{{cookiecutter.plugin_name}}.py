1205f30111b5f4789e3d68a1ff62bdb5b5597fc4
Raphael Pierzina <raphael@hackebrot.de>
2015-10-06 21:43:32 +0200

    Implement a test for an ini option

--- pytest-{{cookiecutter.plugin_name}}/tests/test_{{cookiecutter.plugin_name}}.py
+++ pytest-{{cookiecutter.plugin_name}}/tests/test_{{cookiecutter.plugin_name}}.py
@@ -33,3 +33,31 @@ def test_help_message(testdir):
         '{{cookiecutter.plugin_name}}:',
         '*--foo=DEST_FOO*Set the value for the fixture "bar".',
     ])
+
+
+def test_hello_ini_setting(testdir):
+    testdir.makeini("""
+        [pytest]
+        HELLO = world
+    """)
+
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.fixture
+        def hello(request):
+            return request.config.getini('HELLO')
+
+        def test_hello_world(hello):
+            assert hello == 'world'
+    """)
+
+    result = testdir.runpytest()
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_hello_world PASSED',
+    ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
