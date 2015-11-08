9b46236a7e195b2c4db485d98997df92b6b7fa3e
Pedro Algarvio <pedro@algarvio.me>
2015-11-08 13:14:43 +0000

    Add license to classifiers

--- pytest-{{cookiecutter.plugin_name}}/setup.py
+++ pytest-{{cookiecutter.plugin_name}}/setup.py
@@ -44,6 +44,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         {%- elif cookiecutter.license == "GNU GPL v3.0" -%}
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        {%- elif cookiecutter.license == "Apache Software License 2.0" -%}
+        'License :: OSI Approved :: Apache Software License',
         {%- endif %}
     ],
     entry_points={
