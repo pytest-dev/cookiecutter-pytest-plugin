da238e29efcf8ab4cf68d29d87869031d74d761a
Raphael Pierzina <raphael@hackebrot.de>
2015-11-26 20:34:30 +0100

    Make sure the user does not prepend plugin_name with pytest

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,10 +8,11 @@ import sys
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('pre_gen_project')
 
-PLUGIN_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+PLUGIN_REGEX = r'^(?!pytest)[_a-zA-Z][_a-zA-Z0-9]+$'
 
 plugin_name = '{{cookiecutter.plugin_name}}'
 
 if not re.match(PLUGIN_REGEX, plugin_name):
     logger.error('Invalid value for plugin_name "{}"'.format(plugin_name))
+    logger.debug('Please do not prepend plugin_name with "pytest"!')
     sys.exit(1)
