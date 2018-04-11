b305dd5966600eaa9f1891dfae4fa14e634addf7
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-04-11 15:11:18 +0200

    fix problems with docs initialization when using -f
    fixes #28
    
    * Move all doc sources into a dedicated folder. This add symmetry with
      licenses and hooks and makes tidying up afterwards a no-brainer
    * pull out functionality into a functions that can be used for all
      doc systems
    * set some constants to clarify what stuff means
    * add a simple way to say that a docs file should be placed relative to
      project root instead of relative of docs folder (prepend '/' to name)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,27 +8,39 @@ logger = logging.getLogger('post_gen_project')
 import shutil
 import os
 
+ROOT = os.getcwd()
+DOC_SOURCES = 'doc_sources'
+DOCS = 'docs'
+DOC_TYPE_FILES_MAP = {
+    'mkdocs': ['index.md', '/mkdocs.yml'],
+    'sphinx': ['conf.py', 'index.rst', 'make.bat', 'Makefile']
+}
+
+def move_doc_files(which):
+    logger.info('Initializing docs for %s' % which)
+    if not os.path.exists(DOCS):
+        os.mkdir(DOCS)
+    for item in DOC_TYPE_FILES_MAP[which]:
+        dst, name = (ROOT, item[1:]) if item.startswith('/') else (DOCS, item)
+        src_path = os.path.join(DOC_SOURCES, which, name)
+        dst_path = os.path.join(dst, name)
+        logger.info('Moving %s to %s.' % (src_path, dst_path))
+        if os.path.exists(dst_path):
+            os.unlink(dst_path)
+        os.rename(src_path, dst_path)
 
 {% if cookiecutter.docs_tool == "mkdocs" %}
 
-logger.info('Moving files for mkdocs.')
-os.rename('mkdocs/mkdocs.yml', 'mkdocs.yml')
-shutil.move('mkdocs', 'docs')
-shutil.rmtree('sphinxdocs')
+move_doc_files("mkdocs")
 
 {% elif cookiecutter.docs_tool == "sphinx" %}
 
-logger.info('Moving files for sphinx.')
-shutil.move('sphinxdocs', 'docs')
-shutil.rmtree('mkdocs')
-
-{% else %}
-
-logger.info('Removing all documentation files')
-shutil.rmtree('mkdocs')
-shutil.rmtree('sphinxdocs')
+move_doc_files("sphinx")
 
 {% endif %}
+
+logger.info("Removing all temporary documentation sources")
+shutil.rmtree(DOC_SOURCES)
 
 logger.info('Removing all temporary license files')
 shutil.rmtree('licenses')
