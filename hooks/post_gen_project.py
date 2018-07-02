90b727aaad9802f08b07d9e0778b0c06e67cb0bc
Raphael Pierzina <raphael@hackebrot.de>
2018-07-02 11:55:03 +0200

    Reformat hooks and change variable names

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,40 +6,45 @@ import os
 import shutil
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('post_gen_project')
+logger = logging.getLogger("post_gen_project")
 
-DOC_SOURCES = 'doc_sources'
-ALL_TEMP_FOLDERS = [DOC_SOURCES, 'licenses', 'macros']
-DOC_TYPE_FILES_MAP = {
-    'mkdocs': ['index.md', '/mkdocs.yml'],
-    'sphinx': ['conf.py', 'index.rst', 'make.bat', 'Makefile']
+DOCS_SOURCES = "docs_sources"
+ALL_TEMP_FOLDERS = [DOCS_SOURCES, "licenses", "macros"]
+DOCS_FILES_BY_TOOL = {
+    "mkdocs": ["index.md", "/mkdocs.yml"],
+    "sphinx": ["conf.py", "index.rst", "make.bat", "Makefile"],
 }
 
 
-def move_doc_files(which, map_=DOC_TYPE_FILES_MAP, doc_sources=DOC_SOURCES):
-    if which == 'none':
+def move_docs_files(docs_tool, docs_files, docs_sources):
+    if docs_tool == "none":
         return
+
     root = os.getcwd()
-    docs = 'docs'
-    logger.info('Initializing docs for %s', which)
+    docs = "docs"
+
+    logger.info("Initializing docs for %s", docs_tool)
     if not os.path.exists(docs):
         os.mkdir(docs)
-    for item in map_[which]:
-        dst, name = (root, item[1:]) if item.startswith('/') else (docs, item)
-        src_path = os.path.join(doc_sources, which, name)
+
+    for item in docs_files[docs_tool]:
+        dst, name = (root, item[1:]) if item.startswith("/") else (docs, item)
+        src_path = os.path.join(docs_sources, docs_tool, name)
         dst_path = os.path.join(dst, name)
-        logger.info('Moving %s to %s.', src_path, dst_path)
+
+        logger.info("Moving %s to %s.", src_path, dst_path)
         if os.path.exists(dst_path):
             os.unlink(dst_path)
+
         os.rename(src_path, dst_path)
 
 
-def tidy_up(temp_folders=ALL_TEMP_FOLDERS):
+def remove_temp_folders(temp_folders):
     for folder in temp_folders:
         logger.info("Remove temporary folder: %s", folder)
         shutil.rmtree(folder)
 
 
-if __name__ == '__main__':
-    move_doc_files("{{cookiecutter.docs_tool}}")
-    tidy_up()
+if __name__ == "__main__":
+    move_docs_files("{{cookiecutter.docs_tool}}", DOCS_FILES_BY_TOOL, DOCS_SOURCES)
+    remove_temp_folders(ALL_TEMP_FOLDERS)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,23 +6,23 @@ import re
 import sys
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('pre_gen_project')
+logger = logging.getLogger("pre_gen_project")
 
-PLUGIN_REGEX = r'^(?!pytest)[-_a-zA-Z][-_a-zA-Z0-9]+$'
+PLUGIN_REGEX = r"^(?!pytest)[-_a-zA-Z][-_a-zA-Z0-9]+$"
 
-plugin_name = '{{cookiecutter.plugin_name}}'
+plugin_name = "{{cookiecutter.plugin_name}}"
 
 if not re.match(PLUGIN_REGEX, plugin_name):
     logger.error('Invalid value for plugin_name "{}"'.format(plugin_name))
     logger.debug('Please do not prepend plugin_name with "pytest"!')
     sys.exit(1)
 
-MODULE_REGEX = r'^[a-z][_a-z0-9]+$'
+MODULE_REGEX = r"^[a-z][_a-z0-9]+$"
 
-module_name = '{{cookiecutter.module_name}}'
+module_name = "{{cookiecutter.module_name}}"
 
 if not re.match(MODULE_REGEX, module_name):
-    link = 'https://www.python.org/dev/peps/pep-0008/#package-and-module-names'
-    logger.error('Module name should be pep-8 compliant.')
-    logger.error('  More info: {}'.format(link))
+    link = "https://www.python.org/dev/peps/pep-0008/#package-and-module-names"
+    logger.error("Module name should be pep-8 compliant.")
+    logger.error("  More info: {}".format(link))
     sys.exit(1)
