8c9a90778da891c50ad1ebbcf4ba2ad7a6a21ab3
Hugo <hugovk@users.noreply.github.com>
2019-07-06 23:53:08 +0300

    Upgrade Python syntax with pyupgrade --py3-plus

--- pytest-{{cookiecutter.plugin_name}}/docs_sources/sphinx/conf.py
+++ pytest-{{cookiecutter.plugin_name}}/docs_sources/sphinx/conf.py
@@ -48,9 +48,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pytest-{{cookiecutter.plugin_name}}'
-copyright = u'2015, {{cookiecutter.full_name}}'
-author = u'{{cookiecutter.full_name}}'
+project = 'pytest-{{cookiecutter.plugin_name}}'
+copyright = '2015, {{cookiecutter.full_name}}'
+author = '{{cookiecutter.full_name}}'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -224,8 +224,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pytest-cookiecutterplugin_name.tex', u'pytest-\\{\\{cookiecutter.plugin\\_name\\}\\} Documentation',
-   u'\\{\\{cookiecutter.full\\_name\\}\\}', 'manual'),
+  (master_doc, 'pytest-cookiecutterplugin_name.tex', 'pytest-\\{\\{cookiecutter.plugin\\_name\\}\\} Documentation',
+   '\\{\\{cookiecutter.full\\_name\\}\\}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pytest-cookiecutterplugin_name', u'pytest-{{cookiecutter.plugin_name}} Documentation',
+    (master_doc, 'pytest-cookiecutterplugin_name', 'pytest-{{cookiecutter.plugin_name}} Documentation',
      [author], 1)
 ]
 
@@ -268,7 +268,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pytest-cookiecutterplugin_name', u'pytest-{{cookiecutter.plugin_name}} Documentation',
+  (master_doc, 'pytest-cookiecutterplugin_name', 'pytest-{{cookiecutter.plugin_name}} Documentation',
    author, 'pytest-cookiecutterplugin_name', 'One line description of project.',
    'Miscellaneous'),
 ]
