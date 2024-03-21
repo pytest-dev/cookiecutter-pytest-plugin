2ea74ac79d46ea4fe631d628638afad75732ee0d
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-03-21 14:08:25 +0100

    chore: basic project structure

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,35 +7,7 @@ import shutil
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("post_gen_project")
 
-DOCS_SOURCES = "docs_sources"
-ALL_TEMP_FOLDERS = [DOCS_SOURCES, "licenses", "macros"]
-DOCS_FILES_BY_TOOL = {
-    "mkdocs": ["index.md", "/mkdocs.yml"],
-    "sphinx": ["conf.py", "index.rst", "make.bat", "Makefile"],
-}
-
-
-def move_docs_files(docs_tool, docs_files, docs_sources):
-    if docs_tool == "none":
-        return
-
-    root = os.getcwd()
-    docs = "docs"
-
-    logger.info("Initializing docs for %s", docs_tool)
-    if not os.path.exists(docs):
-        os.mkdir(docs)
-
-    for item in docs_files[docs_tool]:
-        dst, name = (root, item[1:]) if item.startswith("/") else (docs, item)
-        src_path = os.path.join(docs_sources, docs_tool, name)
-        dst_path = os.path.join(dst, name)
-
-        logger.info("Moving %s to %s.", src_path, dst_path)
-        if os.path.exists(dst_path):
-            os.unlink(dst_path)
-
-        os.rename(src_path, dst_path)
+ALL_TEMP_FOLDERS = ["licenses", "macros"]
 
 
 def remove_temp_folders(temp_folders):
@@ -45,5 +17,4 @@ def remove_temp_folders(temp_folders):
 
 
 if __name__ == "__main__":
-    move_docs_files("{{cookiecutter.docs_tool}}", DOCS_FILES_BY_TOOL, DOCS_SOURCES)
     remove_temp_folders(ALL_TEMP_FOLDERS)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,14 +7,8 @@ import sys
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("pre_gen_project")
 
-PLUGIN_REGEX = r"^(?!pytest)[-_a-zA-Z][-_a-zA-Z0-9]+$"
 
 plugin_name = "{{cookiecutter.plugin_name}}"
-
-if not re.match(PLUGIN_REGEX, plugin_name):
-    logger.error('Invalid value for plugin_name "{}"'.format(plugin_name))
-    logger.debug('Please do not prepend plugin_name with "pytest"!')
-    sys.exit(1)
 
 MODULE_REGEX = r"^[a-z][_a-z0-9]+$"
 
